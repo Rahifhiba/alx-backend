f871b365cb3de9c3e29b0eab3273bcd022e29a5c
hepona <hepona10@gmail.com>
2024-11-01 15:37:53 +0100

    1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO caching"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data.values()) > BaseCaching.MAX_ITEMS:
+                tmp = list(self.cache_data)[0]
+                self.cache_data.pop(tmp)
+                print("DISCARD: {}".format(tmp))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
