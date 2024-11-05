dffe77d46857ea52c726ccf5cb121bde4f75cd8e
hepona <hepona10@gmail.com>
2024-11-05 11:03:16 +0100

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""1-app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """class configuration"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello():
+    """hello route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
