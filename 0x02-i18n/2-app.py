dd816b2a14de27e8097fc89eabb0e451aeeeae2f
hepona <hepona10@gmail.com>
2024-11-05 11:21:40 +0100

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,7 +18,7 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector()
+@babel.localeselector
 def get_local():
     """determine the best match with our supported languages."""
     return request.accept_languages.best_match(["en", "fr"])
