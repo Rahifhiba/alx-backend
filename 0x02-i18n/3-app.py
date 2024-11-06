f0e9f0798aa545586b6c6153db895913df347a71
hepona <hepona10@gmail.com>
2024-11-06 01:05:01 +0100

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """3-app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
