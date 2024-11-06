53eec8e5556a5b2acaad1bf60f18566ae6c51f1d
hepona <hepona10@gmail.com>
2024-11-06 03:38:41 +0100

    5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,1 +1,58 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""5-app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """class configuration"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user():
+    """returns a user dictionary or None if
+    the ID cannot be found or if login_as was not passed."""
+    user_id = request.args.get("login_as")
+    if user_id in users:
+        return users[user_id]
+    return None
+
+app.jinja_env.globals['get_user'] = get_user
+@app.before_request
+def before_request():
+    """find a user if any, and set it as a global on flask.g.user."""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages."""
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """route to index page"""
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
